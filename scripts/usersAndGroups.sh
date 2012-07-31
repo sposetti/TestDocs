6a1d11612963e8fbe019237a4dbfead38d5f8e4e
Jeff S <compoze1@yahoo.com>
2012-07-31 11:57:57 -0700

    Update scripts/usersAndGroups.sh

--- scripts/usersAndGroups.sh
+++ scripts/usersAndGroups.sh
@@ -5,25 +5,25 @@
 #
 
 # User which will own the HDFS services.
-export HDFS_USER=hdfs ;
+HDFS_USER=hdfs ;
 
 # User which will own the MapReduce services.
-export MAPRED_USER=mapred ;
+MAPRED_USER=mapred ;
 
 # User which will own the ZooKeeper services.
-export ZOOKEEPER_USER=zookeeper ;
+ZOOKEEPER_USER=zookeeper ;
 
 # User which will own the Hive services.
-export HIVE_USER=hive ;
+HIVE_USER=hive ;
 
 # User which will own the HBase services.
-export HBASE_USER=hbase ;
+HBASE_USER=hbase ;
 
 # User which will own the Oozie services.
-export OOZIE_USER=oozie;
+OOZIE_USER=oozie;
 
 # User which will own the Pig services.
-export PIG_USER=pig ;
+PIG_USER=pig ;
 
 # A common group shared by services.
-export HADOOP_GROUP=hadoop ;
+HADOOP_GROUP=hadoop ;
